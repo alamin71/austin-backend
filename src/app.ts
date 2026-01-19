93929b721c0b875d77bbf777d5d5f0147ea7c2b0
Al-Amin <alamin50cse.miu@gmail.com>
2026-01-19 07:33:39 +0600

    revert to CommonJS: remove ES modules, fix __dirname with path.resolve()

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,8 @@ import { notFound } from './app/middleware/notFound';
 import { welcome } from './utils/welcome';
 import config from './config';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 const app: Application = express();
 
 app.set('view engine', 'ejs');
