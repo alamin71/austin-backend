15f72354a65f042add28e0a2f09e9659446d57d8
Al-Amin <alamin50cse.miu@gmail.com>
2026-01-24 04:41:50 +0600

    updated admin model

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { UserRouter } from '../app/modules/user/user.route.js';
 import { AuthRouter } from '../app/modules/auth/auth.route.js';
+import { AdminRoutes } from '../app/modules/admin/admin.route.js';
 import { StreamRouter } from '../app/modules/stream/stream.route.js';
 import { CategoryRouter } from '../app/modules/category/category.route.js';
 import GiftRouter from '../app/modules/gift/gift.route.js';
@@ -13,6 +14,10 @@ const routes = [
      {
           path: '/auth',
           route: AuthRouter,
+     },
+     {
+          path: '/admin',
+          route: AdminRoutes,
      },
      {
           path: '/user',
