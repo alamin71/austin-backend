fde4ffeabcc997d9a0c0547b443794c91db8c760
Al-Amin <alamin50cse.miu@gmail.com>
2026-01-24 05:58:18 +0600

    ddd

--- src/app/modules/category/category.validation.ts
+++ src/app/modules/category/category.validation.ts
@@ -5,9 +5,8 @@ export const createCategorySchema = z.object({
           title: z.string().min(1, 'Title is required'),
           slug: z.string().toLowerCase().optional(),
           description: z.string().optional(),
-          image: z.string({ required_error: 'Image is required' }),
-          icon: z.string().url().optional(),
-          order: z.number().int().optional(),
+          icon: z.string().optional(),
+          order: z.string().optional(),
      }),
 });
 
@@ -15,10 +14,8 @@ export const updateCategorySchema = z.object({
      body: z.object({
           title: z.string().min(1).optional(),
           description: z.string().optional(),
-          image: z.string().url().optional(),
-          icon: z.string().url().optional(),
-          isActive: z.boolean().optional(),
-          order: z.number().int().optional(),
+          icon: z.string().optional(),
+          order: z.string().optional(),
      }),
 });
 
