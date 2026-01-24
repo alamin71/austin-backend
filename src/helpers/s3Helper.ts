abba1ed6d9af934e6da2ff1a9acbc793b1782dc3
Al-Amin <alamin50cse.miu@gmail.com>
2026-01-24 06:15:58 +0600

    fix: Use Upload from @aws-sdk/lib-storage for S3 file uploads

--- src/helpers/s3Helper.ts
+++ src/helpers/s3Helper.ts
@@ -1,4 +1,5 @@
-import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
 import config from '../config/index.js';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -15,15 +16,18 @@ export const uploadFileToS3 = async (file: Express.Multer.File, folder: string =
 
      const key = `${folder}/${uuidv4()}-${Date.now()}-${file.originalname}`;
 
-     const params = {
-          Bucket: config.aws_s3_bucket_name,
-          Key: key,
-          Body: file.buffer,
-          ContentType: file.mimetype,
-     };
-
      try {
-          await s3Client.send(new PutObjectCommand(params));
+          const upload = new Upload({
+               client: s3Client,
+               params: {
+                    Bucket: config.aws_s3_bucket_name,
+                    Key: key,
+                    Body: file.buffer,
+                    ContentType: file.mimetype,
+               },
+          });
+
+          await upload.done();
           const fileUrl = `https://${config.aws_s3_bucket_name}.s3.${config.aws_region}.amazonaws.com/${key}`;
           return fileUrl;
      } catch (error) {
