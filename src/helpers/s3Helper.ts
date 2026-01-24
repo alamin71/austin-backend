a648b4cd7e79c279e6e5b5b4580b4492ce62c83f
Al-Amin <alamin50cse.miu@gmail.com>
2026-01-24 06:20:19 +0600

    fix: Revert to PutObjectCommand for S3 uploads - works with buffers

--- src/helpers/s3Helper.ts
+++ src/helpers/s3Helper.ts
@@ -1,5 +1,4 @@
-import { S3Client, DeleteObjectCommand } from '@aws-sdk/client-s3';
-import { Upload } from '@aws-sdk/lib-storage';
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
 import config from '../config/index.js';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -16,18 +15,15 @@ export const uploadFileToS3 = async (file: Express.Multer.File, folder: string =
 
      const key = `${folder}/${uuidv4()}-${Date.now()}-${file.originalname}`;
 
-     try {
-          const upload = new Upload({
-               client: s3Client,
-               params: {
-                    Bucket: config.aws_s3_bucket_name,
-                    Key: key,
-                    Body: file.buffer,
-                    ContentType: file.mimetype,
-               },
-          });
+     const params = {
+          Bucket: config.aws_s3_bucket_name,
+          Key: key,
+          Body: file.buffer,
+          ContentType: file.mimetype,
+     };
 
-          await upload.done();
+     try {
+          await s3Client.send(new PutObjectCommand(params));
           const fileUrl = `https://${config.aws_s3_bucket_name}.s3.${config.aws_region}.amazonaws.com/${key}`;
           return fileUrl;
      } catch (error) {
