b699da1a6b339da69d1681262ed1cc0db5127d59
Al-Amin <alamin50cse.miu@gmail.com>
2026-04-10 04:09:37 +0600

    updated get category strem

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -1,4 +1,3 @@
-router.get('/:categoryId/livestreams', categoryController.getLiveStreamsByCategory);
 import express from 'express';
 import categoryController from './category.controller.js';
 
@@ -7,5 +6,6 @@ const router = express.Router();
 // Public routes
 router.get('/', categoryController.getAllCategories);
 router.get('/:categoryId', categoryController.getCategoryById);
+router.get('/:categoryId/livestreams', categoryController.getLiveStreamsByCategory);
 
 export const CategoryRouter = router;
